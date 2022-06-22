a1797945822e9be114e8b89cc729eec3180c6b98
LËØ <87527071+devv-leo@users.noreply.github.com>
2022-06-22 09:09:13 +0100

    Create 0-assignment.js

---
+++ 0-assignment.js
@@ -0,0 +1,9 @@
+// A code that prints my name, height, and country.
+
+var name = "Lekan Olaoye";
+var height = "6'5";
+var country = "Croatia";
+
+document.write("My name is " + name + ". I am " + height + " feets tall. And I am from the country, " + country + ". :)")
+// OR
+console.log(name + ", " + height + ", " + country + ".")
